a1bacee7bfbed2c981eeb235646fc89ea727cfe5
Kelsey Gaetjens <kjgaetjens@gmail.com>
2019-09-04 15:20:45 -0500

    added add book functionality

--- react-admin-site/src/App.js
+++ react-admin-site/src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      title: '',
+      genre: '',
+      publisher: '',
+      year: '',
+      imgurl: ''
+    }
+
+    //add fetch for view all if desired
+  }
+
+  handleAddBookChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  addBook = (e) => {
+    let bookObj = {title: this.state.title, genre: this.state.genre, publisher: this.state.publisher, year: this.state.year, imgurl: this.state.imgurl}
+    fetch('http://localhost:5000/add-book', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(bookObj)
+    })
+  }
+
+
+
+  render() {
+    return <div>
+      <h2>Add a Book</h2>
+      <label>Title</label>
+      <input name="title" type="text" onChange={this.handleAddBookChange}/>
+      <label>Genre</label>
+      <input name="genre" type="text" onChange={this.handleAddBookChange}/>
+      <label>Publisher</label>
+      <input name="publisher" type="text" onChange={this.handleAddBookChange}/>
+      <label>Year</label>
+      <input name="year" type="text" onChange={this.handleAddBookChange}/>
+      <label>Image URL</label>
+      <input name="imgurl" type="text" onChange={this.handleAddBookChange}/>
+      <button onClick={this.addBook}>Add</button>
     </div>
-  );
+  }
 }
+
 
 export default App;
